400a103dbc679297d475d350a5e07f503130737b
Grant Slender <gslender@gmail.com>
2015-09-03 22:28:34 +1000

    initial code commit

---
+++ src/debug.h
@@ -0,0 +1,32 @@
+/**
+ * A simple DEBUG wrapper to simply add/remove debug code in an app.
+ *
+	#define DEBUG // comment out to en/disable
+	#include "debug.h"
+
+	#define DEBUG // comment out to en/disable
+	#define DEBUG_USE_SOFTSERIAL
+	#include "debug.h"
+	D(SoftwareSerial debugSerial;)
+
+    // in setup
+    D(debugSerial = SoftwareSerial(2, 3);)
+    D(debugSerial.begin(9600);)
+ */
+
+
+//#define DEBUG // comment out this line to disable
+#ifdef DEBUG
+	#ifdef DEBUG_USE_SOFTSERIAL
+		#include <SoftwareSerial.h>
+		extern SoftwareSerial debugSerial;
+	#else
+		#define debugSerial Serial
+	#endif
+	//// the following must be in an impl file  D(SoftwareSerial* debugSerial;)
+	#define D(x) x
+	#define UN_D(y)
+#else
+	#define D(x)
+	#define UN_D(y) y
+#endif
---
+++ src/main.cpp
@@ -0,0 +1,17 @@
+#include "Arduino.h"
+
+#define DEBUG // comment out to en/disable
+//#define DEBUG_USE_SOFTSERIAL
+#include "debug.h"
+// D(SoftwareSerial debugSerial;)
+
+#include "MegaSquirt.h"
+
+void setup() {
+	Serial.begin(115200);
+
+}
+
+void loop()
+{
+}
